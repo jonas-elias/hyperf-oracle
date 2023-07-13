1ffa7764180e17eeade92fa6070924c91ae02dfe
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-07-13 00:40:10 +0000

    call function test packagist

---
+++ src/ConfigProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hyperf\Database\Oracle;
+
+/**
+ * class ConfigProvider
+ *
+ * @author jonas-elias
+ */
+class ConfigProvider
+{
+    public function config()
+    {
+        echo 'call function';
+    }
+}
