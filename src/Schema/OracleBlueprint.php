d15722cf587159ca403f2406e6b481afa533baeb
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-11-03 03:18:23 +0000

    style: fix OracleBlueprint php cs

--- src/Schema/OracleBlueprint.php
+++ src/Schema/OracleBlueprint.php
@@ -13,35 +13,37 @@ class OracleBlueprint extends Blueprint
      *
      * @var string
      */
-    public $comment = null;
+    public string $comment = null;
 
     /**
      * Column comments.
      *
      * @var array
      */
-    public $commentColumns = [];
+    public array $commentColumns = [];
 
     /**
      * Database prefix variable.
      *
      * @var string
      */
-    protected $prefix;
+    protected string $prefix;
 
     /**
      * Database object max length variable.
      *
      * @var int
      */
-    protected $maxLength = 30;
+    protected int $maxLength = 30;
 
     /**
      * Set table prefix settings.
      *
-     * @param  string  $prefix
+     * @param string $prefix
+     *
+     * @return void
      */
-    public function setTablePrefix($prefix = '')
+    public function setTablePrefix(?string $prefix = ''): void
     {
         $this->prefix = $prefix;
     }
@@ -49,24 +51,38 @@ class OracleBlueprint extends Blueprint
     /**
      * Set database object max length name settings.
      *
-     * @param  int  $maxLength
+     * @param int $maxLength
+     *
+     * @return void
      */
-    public function setMaxLength($maxLength = 30)
+    public function setMaxLength(?int $maxLength = 30): void
     {
         $this->maxLength = $maxLength;
     }
 
     /**
+     * Create a new nvarchar2 column on the table.
+     *
+     * @param string $column
+     * @param int $length
+     *
+     * @return \Hyperf\Database\Schema\ColumnDefinition
+     */
+    public function nvarchar2(string $column, ?int $length = 255): \Hyperf\Database\Schema\ColumnDefinition
+    {
+        return $this->addColumn('nvarchar2', $column, compact('length'));
+    }
+
+    /**
      * Create a default index name for the table.
      *
-     * @param  string  $type
-     * @param  array  $columns
+     * @param string $type
+     * @param array $columns
+     *
      * @return string
      */
-    protected function createIndexName($type, array $columns)
+    protected function createIndexName($type, array $columns): string
     {
-
-        // if we are creating a compound/composite index with more than 2 columns, do not use the standard naming scheme
         if (count($columns) <= 2) {
             $short_type = [
                 'primary' => 'pk',
@@ -76,14 +92,13 @@ class OracleBlueprint extends Blueprint
 
             $type = isset($short_type[$type]) ? $short_type[$type] : $type;
 
-            $index = strtolower($this->prefix.$this->table.'_'.implode('_', $columns).'_'.$type);
+            $index = strtolower($this->prefix . $this->table . '_' . implode('_', $columns) . '_' . $type);
 
             $index = str_replace(['-', '.'], '_', $index);
             while (strlen($index) > $this->maxLength) {
                 $parts = explode('_', $index);
 
                 for ($i = 0; $i < count($parts); $i++) {
-                    // if any part is longer than 2 chars, take one off
                     $len = strlen($parts[$i]);
                     if ($len > 2) {
                         $parts[$i] = substr($parts[$i], 0, $len - 1);
@@ -93,20 +108,9 @@ class OracleBlueprint extends Blueprint
                 $index = implode('_', $parts);
             }
         } else {
-            $index = substr($this->table, 0, 10).'_comp_'.str_replace('.', '_', microtime(true));
+            $index = substr($this->table, 0, 10) . '_comp_' . str_replace('.', '_', microtime(true));
         }
 
         return $index;
-    }
-
-    /**
-     * Create a new nvarchar2 column on the table.
-     *
-     * @param  string  $column
-     * @param  int  $length
-     */
-    public function nvarchar2($column, $length = 255)
-    {
-        return $this->addColumn('nvarchar2', $column, compact('length'));
     }
 }
