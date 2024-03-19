4e99cd063a0ec219ecff118ffb5d3f3b99da5124
Jonas Elias <jonasdasilvaelias@gmail.com>
2024-03-19 23:21:37 +0000

    test: oracle sql connection methods

--- tests/Database/OracleConnectionTest.php
+++ tests/Database/OracleConnectionTest.php
@@ -2,8 +2,8 @@
 
 namespace HyperfTest\Database\Oracle\Tests\Database;
 
+use Hyperf\Database\Oracle\OracleSqlConnection;
 use Mockery as m;
-use PDO;
 use PHPUnit\Framework\TestCase;
 
 class OracleConnectionTest extends TestCase
@@ -11,5 +11,37 @@ class OracleConnectionTest extends TestCase
     public function tearDown(): void
     {
         m::close();
+    }
+
+    public function testCreateSequence()
+    {
+        $connection = m::mock(OracleSqlConnection::class);
+        $connection->shouldReceive('createSequence')->with('posts_id_seq')->once()->andReturn(true);
+        $this->assertEquals(true, $connection->createSequence('posts_id_seq'));
+    }
+
+    public function testCreateSequenceInvalidName()
+    {
+        $connection = m::mock(OracleSqlConnection::class);
+        $connection->shouldReceive('createSequence')->with(null)->once()->andReturn(false);
+        $this->assertEquals(false, $connection->createSequence(null));
+    }
+
+    public function testDropSequence()
+    {
+        $connection = m::mock(OracleSqlConnection::class);
+        $connection->shouldReceive('dropSequence')->with('posts_id_seq')->once()->andReturn(true);
+        $connection->shouldReceive('checkSequence')->with('posts_id_seq')->once()->andReturn(true);
+        $connection->checkSequence('posts_id_seq');
+        $this->assertEquals(true, $connection->dropSequence('posts_id_seq'));
+    }
+
+    public function testDropSequenceInvalidName()
+    {
+        $connection = m::mock(OracleSqlConnection::class);
+        $connection->shouldReceive('dropSequence')->with(null)->once()->andReturn(false);
+        $connection->shouldReceive('checkSequence')->with(null)->once()->andReturn(true);
+        $connection->checkSequence(null);
+        $this->assertEquals(false, $connection->dropSequence(null));
     }
 }
